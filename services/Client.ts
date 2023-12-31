ae76eeb9ffbb7a8aa6ace560d7bdc5001593a39d
hmtcelik <abhmtcelik@gmail.com>
2023-12-31 21:01:14 +0300

    update create client service

--- services/Client.ts
+++ services/Client.ts
@@ -4,10 +4,7 @@ const createSupaClient = () => {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
   const serviceKey = process.env.SUPA_SERVICE_KEY || "";
 
-  return createClient(
-    supabaseUrl,
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzc2hleGV5anh1dndwaHJ1emxoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMzg0NjIwNCwiZXhwIjoyMDE5NDIyMjA0fQ.eTEBL1obLjjHRAOWtpzK1ERgXcKz2Usks1-4_IQb_co"
-  );
+  return createClient(supabaseUrl, serviceKey);
 };
 
 export default { createSupaClient };
